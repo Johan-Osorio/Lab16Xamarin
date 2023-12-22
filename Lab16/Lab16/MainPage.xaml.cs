cc0deb86ccea6b3447385da9dc037b22e8a5c14d
Johan3105 <johan.osorio@tecsup.edu.pe>
2023-12-21 22:42:23 -0500

    ApiXamarin

---
+++ Lab16/Lab16/MainPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using System.Net;
+using System.Net.Http;
+using Newtonsoft.Json;
+using Lab16.Model;
+
+namespace Lab16
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        public class Libros
+        {
+            public List<Libro> libros { get; set; }
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            var request = new HttpRequestMessage();
+            request.RequestUri = new Uri("https://api-libros-alpha.vercel.app/pages/api/libros.js");
+            request.Method = HttpMethod.Get;
+            request.Headers.Add("Accept", "application/json");
+            var client = new HttpClient();
+            HttpResponseMessage response = await client.SendAsync(request);
+
+            if (response.StatusCode == HttpStatusCode.OK)
+            {
+                var json = await response.Content.ReadAsStringAsync();
+                var libros = JsonConvert.DeserializeObject<Libros>(json);
+                lstLibros.ItemsSource = libros.libros;
+            }
+        }
+    }
+}
